9cc11dcfc497735c5691e1be2ad65a2eae5588a0
mana <manak@kgi.co.th>
2026-07-09 10:44:10 +0700

    LuX: fix SW cache versioning (v2 + skipWaiting + cleanup old)

--- LuX/service-worker.js
+++ LuX/service-worker.js
@@ -1,3 +1,5 @@
+const VERSION = 'v2';
 const ASSETS = ['./', './index.html', './app.css', './app.js', './icon.svg'];
-self.addEventListener('install', e => e.waitUntil(caches.open('v1').then(c => c.addAll(ASSETS))));
+self.addEventListener('install', e => { e.waitUntil(caches.open(VERSION).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== VERSION).map(k => caches.delete(k))))); });
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
